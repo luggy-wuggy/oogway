a7b5b27a815c139eb5f10c8e6cd005fc16281aae
Luqman Abdurrohman <luggyman123@gmail.com>
2022-08-12 16:49:22 -0700

    feat: implement firestore

--- oogway/lib/src/data/facades/authentication_facade.dart
+++ oogway/lib/src/data/facades/authentication_facade.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oogway/src/common/extensions/logger.dart';
@@ -29,4 +30,24 @@ final authenticationFacadeProvider = Provider<AuthenticationFacade>((ref) {
   return AuthenticationFacade(
     firebaseAuth: FirebaseAuth.instance,
   );
+});
+
+final authStateChangesProvider = StreamProvider<User?>(
+  (ref) => ref.watch(authenticationFacadeProvider).authStateChanges,
+);
+
+class OogwayFirestoreDatabase {
+  final FirebaseFirestore database = FirebaseFirestore.instance;
+}
+
+// 3
+final databaseProvider = Provider<OogwayFirestoreDatabase?>((ref) {
+  final auth = ref.watch(authStateChangesProvider);
+
+  // we only have a valid DB if the user is signed in
+  if (auth.asData?.value?.uid != null) {
+    return OogwayFirestoreDatabase();
+  }
+  // else we return null
+  return null;
 });
