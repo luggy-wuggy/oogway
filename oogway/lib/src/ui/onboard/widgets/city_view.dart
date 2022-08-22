a9dd3559d2c24d34fab0e41835fb17d074532266
Luqman Abdurrohman <luggyman123@gmail.com>
2022-08-22 14:55:04 -0700

    feat: address bottom sheet

--- oogway/lib/src/ui/onboard/widgets/city_view.dart
+++ oogway/lib/src/ui/onboard/widgets/city_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oogway/src/common/constants/ui.dart';
+import 'package:oogway/src/common/ui/device_padding.dart';
 import 'package:oogway/src/ui/onboard/controllers/onboard_action_controller.dart';
 import 'package:oogway/src/ui/onboard/controllers/onboard_flow_controller.dart';
 import 'package:oogway/src/ui/widgets/long_button.dart';
@@ -43,6 +44,20 @@ class _CityViewState extends ConsumerState<CityView> {
         OogwayTextField(
           hintText: "City or zip code",
           textEditingController: locationTextController,
+          onTap: () async {
+            await showModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                ),
+                isScrollControlled: true,
+                backgroundColor: OogwayColors.kPrimaryLightColor,
+                builder: (context) {
+                  return const _AddressBottomSheetContent();
+                });
+          },
         ),
         const SizedBox(height: 12),
         const Spacer(flex: 4),
@@ -63,5 +78,85 @@ class _CityViewState extends ConsumerState<CityView> {
   void dispose() {
     locationTextController.dispose();
     super.dispose();
+  }
+}
+
+class _AddressBottomSheetContent extends StatelessWidget {
+  const _AddressBottomSheetContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 16,
+      ),
+      height: size.height - OogwayDevicePadding.top,
+      width: size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Address search',
+            style: TextStyle(
+              color: OogwayColors.kPrimaryDarkColor,
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 12),
+          TextField(
+            style: const TextStyle(
+              color: OogwayColors.kPrimaryDarkColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+            cursorColor: OogwayColors.kPrimaryTransparentDarkColor,
+            textAlign: TextAlign.center,
+            autofocus: true,
+            decoration: InputDecoration(
+              prefixIcon: const Icon(
+                Icons.search,
+                color: OogwayColors.kPrimaryDarkColor,
+              ),
+              suffixIcon: const Icon(
+                Icons.search,
+                color: OogwayColors.kPrimaryLightColor,
+              ),
+              hintText: "City, zip code, or state",
+              hintStyle: TextStyle(
+                color:
+                    OogwayColors.kPrimaryTransparentDarkColor.withOpacity(0.5),
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+              contentPadding: const EdgeInsets.all(10),
+              disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: OogwayColors.kPrimaryTransparentDarkColor,
+                  width: 1,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: OogwayColors.kPrimaryTransparentDarkColor,
+                  width: 1,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: const BorderSide(
+                  color: OogwayColors.kPrimaryTransparentDarkColor,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- oogway/lib/src/ui/widgets/text_field.dart
+++ oogway/lib/src/ui/widgets/text_field.dart
@@ -1,51 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:oogway/src/common/constants/ui.dart';
+import 'package:oogway/src/common/ui/device_padding.dart';
 
 class OogwayTextField extends StatelessWidget {
-  const OogwayTextField(
-      {Key? key, required this.hintText, required this.textEditingController})
-      : super(key: key);
+  const OogwayTextField({
+    Key? key,
+    required this.hintText,
+    required this.textEditingController,
+    this.onTap,
+  }) : super(key: key);
 
   static const double textFieldWidth = 300;
 
   final String hintText;
   final TextEditingController textEditingController;
+  final Function? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: textFieldWidth,
-      child: TextField(
-        controller: textEditingController,
-        style: const TextStyle(
-          color: OogwayColors.kPrimaryLightColor,
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
-        ),
-        cursorColor: OogwayColors.kPrimaryLightColor,
-        textAlign: TextAlign.center,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.all(24),
-          fillColor: OogwayColors.kPrimaryTransparentDarkColor,
-          filled: true,
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: OogwayColors.kPrimaryLightColor.withOpacity(0.5),
+    return GestureDetector(
+      onTap: () async {
+        onTap?.call();
+      },
+      child: SizedBox(
+        width: textFieldWidth,
+        child: TextField(
+          enabled: false,
+          controller: textEditingController,
+          style: const TextStyle(
+            color: OogwayColors.kPrimaryLightColor,
             fontSize: 18,
             fontWeight: FontWeight.w500,
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: const BorderSide(
-              color: OogwayColors.kPrimaryTransparentDarkColor,
-              width: 3,
+          cursorColor: OogwayColors.kPrimaryLightColor,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.all(24),
+            fillColor: OogwayColors.kPrimaryTransparentDarkColor,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(
+              color: OogwayColors.kPrimaryLightColor.withOpacity(0.5),
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
             ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: const BorderSide(
-              color: OogwayColors.kPrimaryTransparentDarkColor,
-              width: 3,
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                color: OogwayColors.kPrimaryTransparentDarkColor,
+                width: 3,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                color: OogwayColors.kPrimaryTransparentDarkColor,
+                width: 3,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                color: OogwayColors.kPrimaryTransparentDarkColor,
+                width: 3,
+              ),
             ),
           ),
         ),
