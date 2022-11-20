d11c60fe03c002424e86ba8efa263339d464b1c4
Luqman Abdurrohman <luggyman123@gmail.com>
2022-11-20 10:55:14 -0800

    added google maps

--- oogway/lib/src/ui/charity_details/charity_details.view.dart
+++ oogway/lib/src/ui/charity_details/charity_details.view.dart
@@ -1,21 +1,13 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:oogway/src/app_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:oogway/src/common/constants/ui.dart';
-import 'package:oogway/src/common/extensions/charity_extension.dart';
-import 'package:oogway/src/common/extensions/double_extension.dart';
 import 'package:oogway/src/models/charity/charity.dart';
 import 'package:oogway/src/ui/charity_details/widgets/details_category_list.dart';
 import 'package:oogway/src/ui/charity_details/widgets/details_header.dart';
 import 'package:oogway/src/ui/charity_details/widgets/details_mission.dart';
 import 'package:oogway/src/ui/charity_details/widgets/details_ratings.dart';
 import 'package:oogway/src/ui/charity_details/widgets/top_app_bar.dart';
-import 'package:oogway/src/ui/controllers/favicon_controller.dart';
-import 'package:oogway/src/ui/onboard/widgets/widgets.dart';
-import 'package:oogway/src/ui/widgets/heart.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class CharityDetailsView extends ConsumerWidget {
   const CharityDetailsView({
@@ -31,35 +23,88 @@ class CharityDetailsView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: OogwayColors.kPrimaryDarkColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: TopAppBar(),
-              ),
-              const SizedBox(height: 24),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: DetailsHeader(charity: charity),
-              ),
-              const SizedBox(height: 24),
-              DetailsCategoryList(charity: charity),
-              const SizedBox(height: 24),
-              DetailsMission(charity: charity),
-              const SizedBox(height: 12),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: DetailsRatings(charity: charity),
-              ),
-            ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: TopAppBar(),
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: DetailsHeader(charity: charity),
+                ),
+                const SizedBox(height: 24),
+                DetailsCategoryList(charity: charity),
+                const SizedBox(height: 24),
+                DetailsMission(charity: charity),
+                const SizedBox(height: 12),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: DetailsRatings(charity: charity),
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: MapView(charity: charity),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class MapView extends StatelessWidget {
+  const MapView({
+    super.key,
+    required this.charity,
+  });
+
+  final Charity charity;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 225,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: const GoogleMap(
+            initialCameraPosition: _kGooglePlex,
+            mapType: MapType.normal,
+            myLocationButtonEnabled: false,
+            scrollGesturesEnabled: false,
+          ),
+        ),
+        Positioned(
+          bottom: 12,
+          right: 12,
+          child: Text(
+            "${charity.mailingAddress?.city ?? ""}, ${charity.mailingAddress?.stateOrProvince ?? ""}",
+            style: const TextStyle(
+              color: OogwayColors.kPrimaryDarkColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- oogway/lib/src/ui/charity_details/widgets/details_mission.dart
+++ oogway/lib/src/ui/charity_details/widgets/details_mission.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:oogway/src/common/constants/ui.dart';
 import 'package:oogway/src/models/charity/charity.dart';
 
-class DetailsMission extends StatelessWidget {
+class DetailsMission extends StatefulWidget {
   const DetailsMission({
     super.key,
     required this.charity,
@@ -11,8 +11,22 @@ class DetailsMission extends StatelessWidget {
   final Charity charity;
 
   @override
+  State<DetailsMission> createState() => _DetailsMissionState();
+}
+
+class _DetailsMissionState extends State<DetailsMission> {
+  bool isMissionExpanded = false;
+
+  void toggleMissionExpansion() {
+    setState(() {
+      isMissionExpanded = !isMissionExpanded;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 24),
@@ -29,25 +43,30 @@ class DetailsMission extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Text(
-            charity.mission ?? "",
+            widget.charity.mission ?? "",
             style: const TextStyle(
               color: OogwayColors.kPrimaryLightColor,
               fontSize: 15,
             ),
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis,
+            maxLines: isMissionExpanded ? null : 3,
+            overflow: isMissionExpanded ? null : TextOverflow.ellipsis,
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Align(
             alignment: Alignment.centerRight,
-            child: Text(
-              "read more",
-              style: TextStyle(
-                color: OogwayColors.kLightBlue,
-                fontSize: 12,
-                decoration: TextDecoration.underline,
+            child: GestureDetector(
+              onTap: () {
+                toggleMissionExpansion();
+              },
+              child: Text(
+                isMissionExpanded ? "read less" : "read more",
+                style: const TextStyle(
+                  color: OogwayColors.kLightBlue,
+                  fontSize: 12,
+                  decoration: TextDecoration.underline,
+                ),
               ),
             ),
           ),
